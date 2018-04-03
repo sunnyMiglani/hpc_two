2ea0c24cdb07c15fe916c7f496839da7a1cafc21
sunnymiglani <sunnymiglani@github.com>
2018-04-03 22:07:43 +0100

    it's done properly, now it worksclear!

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -294,7 +294,7 @@ float func_gatherVelocity(const t_param params,  t_speed *cells, int* obstacles)
         total += my_ans;
         printf("MASTER IS ABOUT TO START RECEIVING FROM PEOPLE \n");
         for(int inp = 1; inp < size; inp++){
-            MPI_Recv(&temp, 1, MPI_SHORT, inp, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&temp, 1, MPI_FLOAT, inp, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             total += temp;
             printf("MASTER RECEIVING FROM WORKER %d VALUE %f \n",inp,temp);
         }
