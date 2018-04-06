7a14b936604ec44083640af36a294afc3127461c
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2018-04-07 01:55:15 +0530

    moved using av_velocity instead of forAll

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -1048,7 +1048,7 @@ float calc_reynolds(const t_param params, t_speed* cells, int* obstacles)
 {
   const float viscosity = 1.f / 6.f * (2.f / params.omega - 1.f);
 
-  return av_velocity_forAll(params, cells, obstacles) * params.reynolds_dim / viscosity;
+  return av_velocity(params, cells, obstacles) * params.reynolds_dim / viscosity;
 }
 
 float total_density(const t_param params, t_speed* cells)
