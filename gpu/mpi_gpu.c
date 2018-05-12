e08967f44c70eb168da72079bffe232858fb3777
sunnymiglani <sunnymiglani@github.com>
2018-05-12 14:20:54 +0100

    builds

--- gpu/mpi_gpu.c
+++ gpu/mpi_gpu.c
@@ -200,7 +200,7 @@ int main(int argc, char* argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  MPI_Aint offset = {0}
+  MPI_Aint offset = {0};
 
   MPI_Datatype this_type = {MPI_FLOAT};
   MPI_Type_create_struct(items,&block_lengths,&offset,&this_type,&cells_struct);
